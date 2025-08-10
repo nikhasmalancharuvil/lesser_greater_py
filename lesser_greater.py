8dbba4ab39f9c118f8b6adedd6c458f1b4546529
nikhas <nikhamalancharuvil@gmail.com>
2025-08-10 16:40:07 +0530

    greater lesser program

---
+++ lesser_greater.py
@@ -0,0 +1,5 @@
+getNum1 = int(input("enter first number"))
+getNum2 = int(input("enter second number"))
+
+result = "Number1 is greater" if getNum1 > getNum2 else "Number 2 is greater"
+print (result)
